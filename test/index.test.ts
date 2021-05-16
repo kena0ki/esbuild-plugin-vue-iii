9a7b0b405ed6ade9d79a7ecc5581f50ca025ba2f
kena0ki <ken3574.25@gmail.com>
2021-05-16 18:10:00 +0900

    add test

---
+++ test/index.test.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import { build } from 'esbuild';
+import { vue3Plugin } from '../src';
+
+describe('generate', () => {
+  test(`001. esm`, async () => {
+    await build({
+      entryPoints: ['playground/vue/index.ts'],
+      bundle: true,
+      sourcemap: true,
+      target: ['es2019'],
+      outdir: 'test/esm/dist',
+      format: 'esm',
+      plugins: [vue3Plugin()],
+    });
+    const indexJs = await fs.promises.readFile('test/esm/dist/index.js','utf-8');
+    expect(indexJs).toMatchSnapshot('indexJs');
+    const indexCss = await fs.promises.readFile('test/esm/dist/index.css','utf-8');
+    expect(indexCss).toMatchSnapshot('indexCss');
+  });
+  test(`002. iife`, async () => {
+    await build({
+      entryPoints: ['playground/vue/index.ts'],
+      bundle: true,
+      sourcemap: true,
+      target: ['es2019'],
+      outdir: 'test/iife/dist',
+      format: 'iife',
+      plugins: [vue3Plugin()],
+    });
+    const indexJs = await fs.promises.readFile('test/iife/dist/index.js','utf-8');
+    expect(indexJs).toMatchSnapshot('indexJs');
+    const indexCss = await fs.promises.readFile('test/iife/dist/index.css','utf-8');
+    expect(indexCss).toMatchSnapshot('indexCss');
+  });
+});
+
