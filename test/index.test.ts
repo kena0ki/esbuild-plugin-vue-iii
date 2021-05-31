99f1e05a82fa8ca93eef3a5047338ceecf45d70d
kena0ki <ken3574.25@gmail.com>
2021-05-31 12:57:13 +0900

    assets test

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,6 +4,7 @@ import { vue3Plugin } from '../src';
 
 describe('generate', () => {
   test(`001. esm`, async () => {
+    await fsp.rmdir('test/esm/dist', { recursive: true });
     await build({
       entryPoints: ['vite/packages/playground/vue/index.ts'],
       bundle: true,
@@ -11,6 +12,11 @@ describe('generate', () => {
       target: ['es2019'],
       outdir: 'test/esm/dist',
       format: 'esm',
+      loader: {
+        '.png': 'file',
+        '.svg': 'file',
+      },
+      publicPath: '/dist',
       plugins: [vue3Plugin()],
     });
     const indexJs = await fsp.readFile('test/esm/dist/index.js','utf-8');
@@ -19,6 +25,7 @@ describe('generate', () => {
     expect(indexCss).toMatchSnapshot('indexCss');
   });
   test(`002. iife`, async () => {
+    await fsp.rmdir('test/iife/dist', { recursive: true });
     await build({
       entryPoints: ['vite/packages/playground/vue/index.ts'],
       bundle: true,
@@ -26,6 +33,11 @@ describe('generate', () => {
       target: ['es2019'],
       outdir: 'test/iife/dist',
       format: 'iife',
+      loader: {
+        '.png': 'file',
+        '.svg': 'file',
+      },
+      publicPath: '/dist',
       plugins: [vue3Plugin()],
     });
     const indexJs = await fsp.readFile('test/iife/dist/index.js','utf-8');
